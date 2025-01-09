aebc71306a83721dfda19482b54f571400b1dcfc
Dilip choudhary <d909abc@gmail.com>
2025-01-09 16:17:30 +0530

    navbar changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaHome, FaHospitalAlt, FaCalendarAlt, FaSignInAlt, FaUserPlus, FaInfoCircle, FaEnvelope } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { signOut } from "firebase/auth";
+import { auth } from "./Firebase";
+import { AuthContext } from "./AuthContext";
+import React, { useContext } from "react";
 
 export default function Navbar() {
+    const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setIsAuthenticated(false);
+      navigate("/login");
+    } catch (err) {
+      console.error("Logout Error:", err.message);
+    }
+  };
     return (
         <nav className="bg-gradient-to-r from-[#0077b6] to-[#00b4d8] shadow-lg">
             <div className="container mx-auto flex items-center justify-between h-16 px-6 lg:px-12">
@@ -34,7 +51,23 @@ export default function Navbar() {
                             <FaInfoCircle /> About Us
                         </li>
                     </Link>
-                    <Link to="/login">
+                    {isAuthenticated ? (
+          <>
+            <Link to="/Profile">
+                        <li className="flex items-center gap-2 text-[#fcefef] font-medium px-1 py-1.5 rounded-lg hover:bg-[#fbb13c] hover:text-[#03045e] transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer shadow-md hover:shadow-lg">
+                        <CgProfile /> Profile
+                        </li>
+                    </Link>
+            <button
+              onClick={handleLogout}
+              className="flex items-center gap-2 text-[#fcefef] font-medium px-1 py-1.5 rounded-lg hover:bg-[#fbb13c] hover:text-[#03045e] transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer shadow-md hover:shadow-lg"
+            >
+              <FaSignInAlt />Logout
+            </button>
+          </>
+        ) : (
+          <>
+           <Link to="/login">
                         <li className="flex items-center gap-2 text-[#fcefef] font-medium px-1 py-1.5 rounded-lg hover:bg-[#fbb13c] hover:text-[#03045e] transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer shadow-md hover:shadow-lg">
                             <FaSignInAlt /> Login
                         </li>
@@ -44,6 +77,8 @@ export default function Navbar() {
                             <FaUserPlus /> Sign Up
                         </li>
                     </Link>
+          </>
+        )}
                 </ul>
             </div>
         </nav>
